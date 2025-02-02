1df9efb943613f43bfd08c7e5ff06091f348c2ac
Dylan Schroder <dylan.schroder31@gmail.com>
2025-02-02 19:51:19 +0000

    restart project again

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MicStreamer from './MicStreamer';
+
+function App() {
+  return (
+    <div className="App">
+      <MicStreamer></MicStreamer>
+    </div>
+  );
+}
+
+export default App;
